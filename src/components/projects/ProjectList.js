9b769a17fce9d3239da65b4a065c2ef7f7af2674
ross-u <cirkovic.uros.srb@gmail.com>
2019-05-25 22:34:12 +0200

    Initial commit

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,51 @@
+// components/projects/ProjectList.js
+
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+import AddProject from './AddProject'; // <== !!!
+
+class ProjectList extends Component {
+	state = { 
+    listOfProjects: [] 
+  };
+
+  getAllProjects = () => {
+    axios.get(`http://localhost:5000/api/projects`)
+    .then((apiResponse) => {
+      this.setState({ listOfProjects: apiResponse.data })
+    })
+  }
+
+  componentDidMount() {
+    //  fetch the data from API befor initial render
+    this.getAllProjects();  
+  }
+
+  render() {
+    const { listOfProjects } = this.state;
+
+    return(
+      <div>         {/* After adding a projects,we will GET all projects again from API  */}
+        <AddProject getData={this.getAllProjects} />   
+        <div>
+          { 
+            listOfProjects.map( (project) => {
+            return (
+              <div key={project._id} className='project'>
+                <Link to={`/projects/${project._id}`}>
+                  <h3>{project.title}</h3>
+                  <p>{project.description} </p>
+                </Link>
+              </div>
+            )})
+          }
+        </div>
+
+      </div>
+    )
+  }
+}
+
+export default ProjectList;
